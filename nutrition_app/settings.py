6dadd272586869b82a324d5cc0291ed40aca4e23
Ubuntu <ubuntu@ip-172-31-4-90.us-east-2.compute.internal>
2025-03-23 17:59:08 +0000

    added config files

--- nutrition_app/settings.py
+++ nutrition_app/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-(svdn=3ge=0^2licp&#r^myk6gfry01*)x*o9=s043pr@vph=e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["18.117.154.80", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["18.222.18.92", "localhost", "127.0.0.1"]
+SESSION_ENGINE = 'django.contrib.sessions.backends.db'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
--- nutrition_app/urls.py
+++ nutrition_app/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
